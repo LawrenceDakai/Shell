d25c9ca87a3be678c6e2e9f47f347eed2ab40040
Lawrence Dakai <mrlawrencedakai@gmail.com>
2017-06-14 22:39:44 +0200

    Create DivisibleBy5.sh

---
+++ DivisibleBy5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter a number " number    # read can output the prompt for you.
+if (( $number % 5 == 0 ))           # no need for brackets
+then
+    echo "Your number is divisible by 5"
+else
+    echo "Your number is not divisible by 5"
+fi
+
+# credit: https://superuser.com/users/310/dennis-williamson
